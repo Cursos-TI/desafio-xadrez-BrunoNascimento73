873cc88b536d923fd5fd3a87ab0be668f161c4d4
BrunoNascimento73 <202601126962@alunos.estacio.br>
2026-02-26 02:30:53 +0000

    Função recursiva para as movimentações

--- xadrez.c
+++ xadrez.c
@@ -1,52 +1,78 @@
 #include <stdio.h>
 
 
-int main() {
-    // Implementação de Movimentação da Torre
-    printf("--------------------------\n");
-    printf("Moverei minha torre!\n");
-    for (int i = 0; i < 5; i++) {
-      printf("Direita!\n");
-    }
-    printf("\n");
+void movimentodiagnalD(){
+  for (int i = 0; i < 6; i++) {
+    printf("direita\n");
+    printf("Cima\n");
+  }
+}
 
-    // Implementação de Movimentação do Bispo
-    printf("Irei mover meu bispo!\n");
-    for (int i = 0; i < 6; i++) {
-      printf("direita\n");
-      printf("Cima\n");
-    }
-    printf("\n");
-
-    // Implementação de Movimentação da Rainha
-    printf("irei mover meu minha rainha\n");
-    for (int i = 0; i < 4; i++) {
+void movimentodiagnale(){
+  for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++)
+      {
+        printf("esquerda\n");
+      }
       printf("cima\n");
-      printf("esquerda\n");
+      
     }
-    printf("\n");
+}
 
-    // Implementação de Movimentação do cavalo
-    printf("Irei mover meu cavalo\n");
-    // While
-    int movimentocompleto = 1 ;
+void movimentohorizontal(){ // função recursiva
+  for (int i = 0; i < 5; i++) {
+    printf("Direita!\n");
+  }
+}
+
+void MovimentoCavaloWhile(){
+  int movimentocompleto = 1 ;
     while (movimentocompleto--) {
       for (int i = 0; i < 2; i++) {
         printf("Cima\n");
       }
       printf("esquerda\n");
     }
-   
-    
-    // Do while
+}
+
+void MovimentoCavaloDo(){
     int i = 0;
-    printf("Irei Mover meu cavalo\n");
     do // vai ser executado
     {
       printf("Baixo\n");
       i++;
     } while ( i < 2); //enquanto condição for verdadeira
     printf("direita\n");
+}
+
+int main() {
+    // Implementação de Movimentação da Torre
+    printf("--------------------------\n");
+    printf("Moverei minha torre!\n");
+    movimentohorizontal();
+    printf("\n");
+
+    // Implementação de Movimentação do Bispo
+    printf("Irei mover meu bispo!\n");
+    movimentodiagnalD();
+    printf("\n");
+
+    // Implementação de Movimentação da Rainha
+    printf("irei mover meu minha rainha\n");
+    movimentodiagnale();
+    printf("\n");
+
+    // Implementação de Movimentação do cavalo
+    printf("Irei mover meu cavalo\n");
+    // While
+    MovimentoCavaloWhile();
+    printf("\n");
+   
+    
+    // Do while
+    printf("Irei Mover meu cavalo\n");
+    MovimentoCavaloDo(); 
+    printf("\n");
     
     
 
